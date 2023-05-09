5f1cb0fce8ca887fa8b019418c08608db4e279b5
Javi Tejero <javitejero@93gmail.com>
2023-05-09 13:24:42 +0200

    metodoInsert1

--- Hoja11Cuestionario/src/main/java/com/example/hoja11cuestionario/Transformar.java
+++ Hoja11Cuestionario/src/main/java/com/example/hoja11cuestionario/Transformar.java
@@ -8,4 +8,22 @@ public class Transformar {
         return sql;
     }
 
+    public static String insertPregunta1(int id_preguntaV, String preguntaV, String respuestaV,
+                                         String personaje1, int valor1){
+
+        String sql;
+        sql = "INSERT INTO `preguntas4resp` (`id`, `id_pregunta`, `respuesta`, `"+personaje1+"`) VALUES (NULL, "+id_preguntaV+", "+respuestaV+", "+valor1+") ";
+        return sql;
+
+    }
+    public static String insertPregunta2(int id_preguntaV, String preguntaV, String respuestaV,
+                                         String personaje1, int valor1,String personaje2, int valor2){
+
+        String sql;
+        sql = "INSERT INTO `preguntas4resp` (`id`, `id_pregunta`, `respuesta`, `"+personaje1+"`) VALUES (NULL, "+id_preguntaV+", "+respuestaV+", "+valor1+") ";
+        return sql;
+
+    }
+
+
 }
